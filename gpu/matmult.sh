050426e68982a30368c4844ae8dc0dcfca121d66
Rai Cho <rai.cho@linecorp.com>
2014-10-10 17:49:21 +0900

    GPU

--- gpu/matmult.sh
+++ gpu/matmult.sh
@@ -1,2 +1,3 @@
+PATH=$PATH:/usr/local/cuda/bin
 nvcc -g -G -I/usr/local/cuda/include -Xcompiler "-I/Library/Frameworks/R.framework/Resources/include -fpic" -c matmult.cu -o matmult.o -arch=sm_30
 nvcc -shared -Xlinker "-L/Library/Frameworks/R.framework/Resources/lib -lR" -L/usr/local/cuda/lib -lcublas matmult.o -o matmult.so
