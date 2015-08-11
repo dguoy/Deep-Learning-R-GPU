b82b8270158273ee81674a55602fe2906424585f
Rai Cho <rai.cho@linecorp.com>
2015-08-11 09:09:32 +0900

    vectorization

--- deeplearning/denoisingAutoencoder.R
+++ deeplearning/denoisingAutoencoder.R
@@ -5,12 +5,12 @@ denoisingAutoencoderCost <- function(theta, visibleSize, hiddenSize, data) {
   W = matrix(theta[1 : (hiddenSize*visibleSize)], hiddenSize, visibleSize)
   bhid = theta[(hiddenSize*visibleSize+1) : (hiddenSize*visibleSize+hiddenSize)]
   bvis = theta[(hiddenSize*visibleSize+hiddenSize+1) : length(theta)]
-  
+
   z2 <- W %*% data + bhid
   a2 <- sigmoid(z2)
   z3 <- t(W) %*% a2 + bvis
   a3 <- sigmoid(z3)
-  
+
   L <- -colSums(data * log(a3) + (1 - data) * log(1 - a3))
   return (mean(L))
 }
@@ -29,19 +29,14 @@ denoisingAutoencoderGrad <- function(theta, visibleSize, hiddenSize, data) {
   LGrad <- data / a3 - (1 - data) / (1 - a3)
   a3Grad <- a3 * (1 - a3)
   a2Grad <- a2 * (1 - a2)
+  a3GradLGrad <- a3Grad * LGrad
 
-  WGrad <- matrix(0, hiddenSize, visibleSize)
-
-  for(i in 1:m) {
-    WGrad <- WGrad -
-      (a2Grad[, i] %*% t(data[, i])) * as.vector(W %*% (LGrad[, i] * a3Grad[, i])) -
-      t(t(a2[, i] %*% t(a3Grad[, i])) * LGrad[, i])
-  }
+  WGrad <- -(a2Grad * (W %*% a3GradLGrad)) %*% t(data) - a2 %*% t(a3GradLGrad)
   WGrad <- WGrad / m
 
-  bhidGrad <- -rowMeans((W %*% (LGrad * a3Grad)) * a2Grad)
-  bvisGrad <- -rowMeans(LGrad * a3Grad)
-  
+  bhidGrad <- -rowMeans((W %*% a3GradLGrad) * a2Grad)
+  bvisGrad <- -rowMeans(a3GradLGrad)
+
   return (c(WGrad, bhidGrad, bvisGrad))
 }
 #************************************************ With Object-oriented *******************************************************************************
--- denoisingAutoEncoderExercise.R
+++ denoisingAutoEncoderExercise.R
@@ -1,27 +1,115 @@
 # Denoising Autoencoder
 source('deeplearning/common.R')
+source('deeplearning/softmax.R')
 source('deeplearning/denoisingAutoencoder.R')
 
-images <- readMat("data/IMAGES.mat")[[1]]
+hiddenSize <- 500
+batchSize <- 20
+alpha <- 0.1
 
-maxIter <- 2000
-visibleSize <- 8*8
-hiddenSize <- 25
+trainData <- loadImageFile('data/train-images-idx3-ubyte')
 
-patches <- sampleImages(images)
+m <- ncol(trainData)
+inputSize <- nrow(trainData)
+numbatches <- m / batchSize
 
-r <- 4 * sqrt(6) / sqrt(hiddenSize+visibleSize)
-W <- runif(hiddenSize * visibleSize, min = -r, max = r)
+r <- 4 * sqrt(6) / sqrt(hiddenSize+inputSize)
+W <- matrix(runif(hiddenSize * inputSize) * 2 * r - r, hiddenSize, inputSize)
+b <- rep(0, inputSize)
+c <- rep(0, hiddenSize)
+theta <- c(W, b, c)
+
+for(l in 1:15) {
+  kk <- sample(1:m)
+  cost <- 0
+  for(i in 1:numbatches) {
+    dat <- trainData[, kk[((i - 1)*batchSize+1) : (i*batchSize)]]
+    dat <- rbinom(batchSize * inputSize, 1, 0.7) * dat
+
+    cost <- cost + denoisingAutoencoderCost(theta, inputSize, hiddenSize, dat)
+    theta <- theta -  alpha * denoisingAutoencoderGrad(theta, inputSize, hiddenSize, dat)
+  }
+  print(sprintf("At iterate %s = %s", l, cost / numbatches))
+}
+W = matrix(theta[1 : (hiddenSize*inputSize)], hiddenSize, inputSize)
+displayNetwork(W[1:100, ])
+
+
+trainData <- loadImageFile('data/train-images-idx3-ubyte')
+trainLabels <- loadLabelFile('data/train-labels-idx1-ubyte')
+numLabels <- length(table(trainLabels))
+testData <- loadImageFile('data/t10k-images-idx3-ubyte')
+testLabels <- loadLabelFile('data/t10k-labels-idx1-ubyte')
+
+trainFeatures <- feedForwardAutoencoder(theta, hiddenSize, inputSize, trainData)
+testFeatures <- feedForwardAutoencoder(theta, hiddenSize, inputSize, testData)
+
+softmaxLambda <- 1e-4
+softmaxTheta <- 0.005 * runif(numLabels * hiddenSize)
+softmaxOptTheta <- optim(softmaxTheta,
+                         function(theta) softmaxCost(theta, numLabels, hiddenSize, softmaxLambda, trainFeatures, trainLabels),
+                         function(theta) softmaxGrad(theta, numLabels, hiddenSize, softmaxLambda, trainFeatures, trainLabels),
+                         method = "L-BFGS-B", control = list(trace = 3, maxit = 500))$par
+
+softmaxPredict(softmaxOptTheta, testFeatures, testLabels)
+softmaxPredict(softmaxOptTheta, trainFeatures, trainLabels)
+
+
+#**************************************************************************************************
+r <- 4 * sqrt(6) / sqrt(hiddenSize+inputSize)
+W <- matrix(runif(hiddenSize * inputSize) * 2 * r - r, hiddenSize, inputSize)
 bhid <- rep(0, hiddenSize)
-bvis <- rep(0, visibleSize)
-theta <- c(W, bhid, bvis)
+bvis <- rep(0, inputSize)
 
-#************************************************ With method ***************************************************************************************
-optimTheta <- optim(theta,
-		function(theta) denoisingAutoencoderCost(theta, visibleSize, hiddenSize, patches),
-		function(theta) denoisingAutoencoderGrad(theta, visibleSize, hiddenSize, patches),
-		method = "L-BFGS-B", control = list(trace = 3, maxit = maxIter))$par
+for(l in 1:10) {
+  kk <- sample(1:m)
+  cost <- 0
+  for(i in 1:numbatches) {
+    data <- trainData[, kk[((i - 1)*batchSize+1) : (i*batchSize)]]
+    data <- rbinom(batchSize * inputSize, 1, 0.7) * data
 
-W <- matrix(optimTheta[1 : (hiddenSize*visibleSize)], hiddenSize, visibleSize)
-displayNetwork(W)
+    z2 <- W %*% data + bhid
+    a2 <- sigmoid(z2)
+    z3 <- t(W) %*% a2 + bvis
+    a3 <- sigmoid(z3)
+
+    LGrad <- data / a3 - (1 - data) / (1 - a3)
+    a3Grad <- a3 * (1 - a3)
+    a2Grad <- a2 * (1 - a2)
+    a3GradLGrad <- a3Grad * LGrad
+    Wa3GradLGrad <- W %*% a3GradLGrad
+
+    WGrad <- -(a2Grad * Wa3GradLGrad) %*% t(data) - a2 %*% t(a3GradLGrad)
+    W <- W - alpha * WGrad / batchSize
+
+    bhidGrad <- -rowMeans(Wa3GradLGrad * a2Grad)
+    bhid <- bhid - alpha * bhidGrad
+
+    bvisGrad <- -rowMeans(a3GradLGrad)
+    bvis <- bvis - alpha * bvisGrad
+
+    L <- -colSums(data * log(a3) + (1 - data) * log(1 - a3))
+    cost <- cost + mean(L)
+  }
+  print(sprintf("At iterate %s = %s", l, cost / numbatches))
+}
+
+trainData <- loadImageFile('data/train-images-idx3-ubyte')
+trainLabels <- loadLabelFile('data/train-labels-idx1-ubyte')
+numLabels <- length(table(trainLabels))
+testData <- loadImageFile('data/t10k-images-idx3-ubyte')
+testLabels <- loadLabelFile('data/t10k-labels-idx1-ubyte')
+
+trainFeatures <- feedForwardAutoencoder(c(W, rep(0, hiddenSize)), hiddenSize, inputSize, trainData)
+testFeatures <- feedForwardAutoencoder(c(W, rep(0, hiddenSize)), hiddenSize, inputSize, testData)
+
+softmaxLambda <- 1e-4
+softmaxTheta <- 0.005 * runif(numLabels * hiddenSize)
+softmaxOptTheta <- optim(softmaxTheta,
+                         function(theta) softmaxCost(theta, numLabels, hiddenSize, softmaxLambda, trainFeatures, trainLabels),
+                         function(theta) softmaxGrad(theta, numLabels, hiddenSize, softmaxLambda, trainFeatures, trainLabels),
+                         method = "L-BFGS-B", control = list(trace = 3, maxit = 500))$par
+
+softmaxPredict(softmaxOptTheta, testFeatures, testLabels)
+softmaxPredict(softmaxOptTheta, trainFeatures, trainLabels)
 
